df9c17f310fd130e9def8b8557dc1c9df7b3750f
Tee <sotomi.tayo@gmail.com>
2023-02-14 13:04:19 -0600

    Star Wars API

---
+++ StarWarsAPI/Models/StarWarsDAL.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace StarWarsAPI.Models
+{
+    public class StarWarsDAL
+    {
+        public static StarwarsModel GetStarWars(string input)
+        {
+            //Adjust here
+            //setup
+            string url = $"https://swapi.dev/api/people/?search={input}";
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            //save the response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Adjust here
+            //convert to C#
+            StarwarsModel result = JsonConvert.DeserializeObject<StarwarsModel>(JSON);
+            return result;
+        }
+
+       
+    }
+}
